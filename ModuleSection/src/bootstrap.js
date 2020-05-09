5e774a03f112b0546510a85a4d3ba0669d0eee27
HajaSC <hajasc@protonmail.com>
2020-05-08 23:36:39 -0400

    Adds Default and Named Imports

--- ModuleSection/src/bootstrap.js
+++ ModuleSection/src/bootstrap.js
@@ -1,4 +1,10 @@
-import * as helper from './helper';
+//import * as helper from './helper';
+//import {multiply, greeting} from './helper';
+import navigation, {greeting, multiply} from './navigation';
 
-console.log(helper.greeting);
-console.log(helper.multiply(5,6));
+console.log(greeting);
+console.log(multiply(5,6));
+console.log(navigation());
+
+
+
