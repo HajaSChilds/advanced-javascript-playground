77bbea36f910f667c976eac2dafea8b3bc64b7c9
HajaSC <hajasc@protonmail.com>
2020-05-12 02:29:07 -0400

    Moment Js

--- ModuleSection/src/bootstrap.js
+++ ModuleSection/src/bootstrap.js
@@ -1,6 +1,6 @@
 //import * as helper from './helper';
 //import {multiply, greeting} from './helper';
-import navigation, {greeting, multiply} from './navigation';
+import navigation, { greeting, multiply } from './navigation';
 
 console.log(greeting);
 console.log(multiply(5,6));
